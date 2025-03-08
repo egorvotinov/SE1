9792b7d5611d2e79aa8fb80e3a89ab4f7d7fda07
Egor Votinov <EgorVotinov@gmail.com>
2025-03-08 15:55:57 +0500

    вывод суммы чисел

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-     int num1, num2;
-     cout << "¬ведите первое целое число: ";
-     cin >> num1;
-     cout << "¬ведите второе целое число: ";
-     cin >> num2;
-     return 0;
- }
+    int num1, num2;
+    cout << "¬ведите первое целое число: ";
+    cin >> num1;
+    cout << "¬ведите второе целое число: ";
+    cin >> num2;
+    cout << "—умма чисел: " << num1 + num2 << endl;
+    return 0;
+}
